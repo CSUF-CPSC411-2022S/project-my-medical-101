8c0c4b7694b0500a3664312abb82eb1aebb69081
gomezb636 <gomezb636@csu.fullerton.edu>
2022-02-21 23:08:28 -0800

    Created basic outline for myChart

---
+++ My Medical 101/myChart.swift
@@ -0,0 +1,12 @@
+//
+//  myChart.swift
+//  My Medical 101
+//
+//  Created by Brenda Gomez on 2/21/22.
+//
+
+import Foundation
+
+struct Chart {
+    // add code on my chart for medical history
+}
