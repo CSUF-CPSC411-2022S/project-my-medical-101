cf5e7762c1ba09792728f002ea43ee773d4ad1df
gomezb636 <gomezb636@csu.fullerton.edu>
2022-02-21 22:38:40 -0800

    Initial Commit

---
+++ My Medical 101/My_Medical_101App.swift
@@ -0,0 +1,17 @@
+//
+//  My_Medical_101App.swift
+//  My Medical 101
+//
+//  Created by Brenda Gomez on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct My_Medical_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My Medical 101UITests/My_Medical_101UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  My_Medical_101UITestsLaunchTests.swift
+//  My Medical 101UITests
+//
+//  Created by Brenda Gomez on 2/21/22.
+//
+
+import XCTest
+
+class My_Medical_101UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
