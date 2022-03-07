fd67cf40f31409f5ce9c8b76dcef2ed7cf68cc4f
Vibha Rajagopalan <vibha@csu.fullerton.edu>
2022-03-07 00:39:35 -0800

    Schedule appt UI (still in progress)

--- My Medical 101/ContentView.swift
+++ My Medical 101/ContentView.swift
@@ -8,14 +8,220 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name: String = ""
+    @State var buttonClicked = false
+    @State var makeAppt = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            VStack(alignment: .center) {
+                VStack (alignment: .leading){
+                    Text("Schedule Your Appointment").modifier(SchedulePageTitle())
+                }.padding([.top, .leading])
+                VStack(alignment: .leading) {
+                    Text("March 7:")
+                    HStack{
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                }
+                
+                VStack (alignment: .leading) {
+                    Text("March 8:")
+                    HStack{
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                }
+                
+                VStack (alignment: .leading) {
+                    Text("March 9:")
+                    HStack{
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                }
+                
+                VStack (alignment: .leading) {
+                    Text("March 10:")
+                    HStack{
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                }
+                
+                VStack (alignment: .leading) {
+                    Text("March 11:")
+                    HStack{
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 6:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:00 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                        
+                        Button(action: {buttonClicked=true}) {
+                            Text(" 7:30 ").font(.system(size: geometry.size.width/30))
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                                .frame(minWidth: geometry.size.width/5)
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                    .padding(.bottom)
+                }
+                
+                if buttonClicked {
+                    Text("You've selected a time for your appointment.\nPlease click the \"Make Appointment\" button below to schedule your appointment!")
+                        .foregroundColor(Color.white).padding(.horizontal)
+                }
+                Button(action: {makeAppt=true}) {
+                    Text(" Make Appointment ").font(.system(size: geometry.size.width/25))
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 8).fill(Color.yellow))
+                        .frame(minWidth: geometry.size.width/4)
+                }.padding(.top).buttonStyle(PlainButtonStyle())
+                
+                if makeAppt {
+                    if buttonClicked{
+                        Text("You've made your appointment!")
+                            .foregroundColor(Color.white).padding(.horizontal)
+                    }
+                    else{
+                        Text("Please choose a time for your appointment.").foregroundColor(Color.white).padding(.horizontal)
+                    }
+                }
+            }
+        }.modifier(PageBackground())
+    }
+}
+struct SchedulePageTitle: ViewModifier {
+   func body(content: Content) -> some View {
+        content
+           .font(.custom("Courier New", size: 25))
+           .foregroundColor(Color.white)
+           .padding()
+           .background(Color.teal)
+           .cornerRadius(10)
+           .frame(alignment: .center)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct PageBackground: ViewModifier {
+   func body(content: Content) -> some View {
+        content
+           .background(Color.primary)
     }
 }
