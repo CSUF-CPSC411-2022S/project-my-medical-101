64ce3d0300e39006e7cc388e51add48eaa37e0de
Vibha Rajagopalan <vibha@csu.fullerton.edu>
2022-03-21 17:34:08 -0700

    help page modifications

--- My Medical 101/helpPageView.swift
+++ My Medical 101/helpPageView.swift
@@ -17,20 +17,22 @@ struct HelpPage : View {
                         .modifier(helpTitle()).padding(.bottom)
                 }
                 VStack {
-                    Text("1. First, if you have not already, please create an account.\n2. If you already have an account, login with your username and password.\n3. After logging in and getting to the homepage, you can either Schedule an appointment or view your medical chart.\n")
+                    Text("1. First, if you have not already, \nplease create an account.\n\n2. If you already have an account, login with your username and password.\n\n3. After logging in and getting to the homepage, you can either Schedule an appointment or view your medical chart.")
                         .multilineTextAlignment(.leading)
-                        .padding(.leading)
+                        .padding([.top, .leading])
                     Spacer()
                 }.modifier(instructions())
                 VStack {
-                    Text("Scheduling an appointment:\n1. After navigating to the \"Schedule Appointments\" section, you will see the dates on which the doctor is available to meet with you.\n2.Below each date will be the times they are available at. \n3.Please make sure to select one of the times and then click on \"Make Appointment\"\n")
+                    Text("Scheduling an appointment:\n").bold().padding(.top).font(.custom("Times New Roman", size: 14))
+                    Text("1. After navigating to the \"Schedule Appointments\" section, you will see the dates on which the doctor is available to meet with you.\n\n2. Below each date will be the times they are available at. \n\n3. Please make sure to select one of the times and then click on \"Make Appointment\"")
                         .multilineTextAlignment(.leading)
-                        .padding(.leading)
+                        .padding([.leading])
                     Spacer()
                 }.modifier(instructions())
                 VStack {
-                    Text("View your Medical Chart:\n1. After Navigating to this page, you will see all your medical details listed here.\n2. Click on \"Medications\" to view any medications that your doctor may have prescribed to you.").multilineTextAlignment(.leading)
-                        .padding(.leading)
+                    Text("View your Medical Chart:\n").bold().padding(.top).font(.custom("Times New Roman", size: 14))
+                    Text("1. After navigating to this page, you will see all your medical details listed here.\n\n2. Click on \"Medications\" to view any medications that your doctor may have prescribed to you.").multilineTextAlignment(.leading)
+                        .padding([.leading])
                     Spacer()
                 }.modifier(instructions())
             }
@@ -57,7 +59,7 @@ struct helpTitle: ViewModifier {
 struct instructions: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .font(.custom("Times New Roman", size: 12))
+            .font(.custom("Times New Roman", size: 13))
             .frame(width: 360, height: 180)
             .background(Color("lightPastelBlue"))
             .cornerRadius(25)
